b37ba63a9fc27250910ba11965c59600fcc09b09
Alec Jo <alecjo@Alecs-MacBook-Pro.local>
2020-07-19 18:19:26 -0700

    first commit- basic app functionality

---
+++ constants/mocks.js
@@ -0,0 +1,10 @@
+const profile = {
+  username: "alecjo",
+  location: "United States",
+  email: "alecjo@gmail.com",
+  avatar: require("../assets/images/avatar.png"),
+  notifications: true,
+  newsletter: false
+};
+
+export { profile };
---
+++ screens/Dashboard.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import {
+  Dimensions,
+  Image,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity
+} from "react-native";
+
+import { Button, Block, Text } from "../components";
+import { theme, mocks } from "../constants";
+
+const { width } = Dimensions.get("window");
+
+class Dashboard extends Component {
+  state = {
+    active: "Overview",
+    categories: ["Overview", "Activity", "Groups"]
+  };
+
+
+  handleTab = tab => {
+    this.setState({ active: tab });
+  };
+
+  renderTab(tab) {
+    const { active } = this.state;
+    const isActive = active === tab;
+
+    return (
+      <TouchableOpacity
+        key={`tab-${tab}`}
+        onPress={() => this.handleTab(tab)}
+        style={[styles.tab, isActive ? styles.active : null]}
+      >
+        <Text size={16} medium gray={!isActive} secondary={isActive}>
+          {tab}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+
+  render() {
+    const { profile, navigation } = this.props;
+    const tabs = ["Overview", "Activity", "Groups"];
+
+    return (
+      <Block>
+        <Block flex={false} row center space="between" style={styles.header}>
+          <Text h1 bold>
+            Dashboard
+          </Text>
+          <Button onPress={() => navigation.navigate("Settings")}>
+            <Image source={profile.avatar} style={styles.avatar} />
+          </Button>
+        </Block>
+
+        <Block flex={false} row style={styles.tabs}>
+          {tabs.map(tab => this.renderTab(tab))}
+        </Block>
+
+        <Text>
+          TODO  
+        </Text>
+      </Block>
+    );
+  }
+}
+
+Dashboard.defaultProps = {
+  profile: mocks.profile,
+  categories: mocks.categories
+};
+
+export default Dashboard;
+
+const styles = StyleSheet.create({
+  header: {
+    paddingHorizontal: theme.sizes.base * 2
+  },
+  avatar: {
+    height: theme.sizes.base * 2.2,
+    width: theme.sizes.base * 2.2
+  },
+  tabs: {
+    borderBottomColor: theme.colors.gray2,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginVertical: theme.sizes.base,
+    marginHorizontal: theme.sizes.base * 2
+  },
+  tab: {
+    marginRight: theme.sizes.base * 2,
+    paddingBottom: theme.sizes.base
+  },
+  active: {
+    borderBottomColor: theme.colors.secondary,
+    borderBottomWidth: 3
+  },
+  categories: {
+    flexWrap: "wrap",
+    paddingHorizontal: theme.sizes.base * 2,
+    marginBottom: theme.sizes.base * 3.5
+  },
+  category: {
+    // this should be dynamic based on screen width
+    minWidth: (width - theme.sizes.padding * 2.4 - theme.sizes.base) / 2,
+    maxWidth: (width - theme.sizes.padding * 2.4 - theme.sizes.base) / 2,
+    maxHeight: (width - theme.sizes.padding * 2.4 - theme.sizes.base) / 2
+  }
+});
